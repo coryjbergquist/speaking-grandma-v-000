e340a50205181f6866f47ed05dc735ccfad3c276
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-16 17:33:30 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase == (phrase.upcase && "I LOVE YOU GRANDMA!"
+  if phrase == (phrase.upcase && "I LOVE YOU GRANDMA!")
     "I LOVE YOU TOO PUMPKIN!"
     elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
