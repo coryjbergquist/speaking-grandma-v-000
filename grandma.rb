3f455ec4545735bf5f5959d175e138b45b334435
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-16 17:18:52 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -13,10 +13,10 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(phrase)
-  if phrase == phrase.upcase
-    'I LOVE YOU TOO PUMPKIN!'
-    elsif phrase == 'WHAT DID YOU EAT TODAY?'
+  if phrase == 'WHAT DID YOU EAT TODAY?'
     "NO, NOT SINCE 1938!"
+  elsif phrase == phrase.upcase
+    'I LOVE YOU TOO PUMPKIN!'
 else
   "HUH?! SPEAK UP, SONNY!"
 end
