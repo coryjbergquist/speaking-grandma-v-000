12e655a3f1931d61ad86d5687ad187a53fcfe3f5
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-05-16 17:33:14 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,6 +14,7 @@
 
 def speak_to_grandma(phrase)
   if phrase == (phrase.upcase && "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO PUMPKIN!"
     elsif phrase == phrase.upcase
     "NO, NOT SINCE 1938!"
   else 
